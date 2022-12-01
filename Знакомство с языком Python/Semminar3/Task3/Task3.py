de5d903730baf7f091f871b15c1c1153d46a5c1f
Artem Eremenko <qgren@ukr.net>
2022-12-02 00:44:47 +0200

    Completed Task3

---
+++ Знакомство с языком Python/Semminar3/Task3/Task3.py
@@ -0,0 +1,30 @@
+# 3'. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным
+# значением дробной части элементов.
+# *Пример:*
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+
+work_ls = input("Введите элементы списка (вещественные числа через .) через пробел: ").split(' ')
+print(work_ls)
+max_len = 1
+result_ls = []
+for elem in work_ls:
+    start = elem.find('.') + 1
+    if start > 0:
+        result_ls.append(elem[start:])
+    if len(elem[start:]) > max_len:
+        max_len = len(elem[start:])
+
+for i in range(len(result_ls)):
+    difference = max_len - len(result_ls[i])
+    result_ls[i] += '0'*difference
+    result_ls[i] = int(result_ls[i])
+
+max_elem = result_ls[0]
+min_elem = result_ls[0]
+for elem in result_ls:
+    if elem > max_elem:
+        max_elem = elem
+    if elem < min_elem:
+        min_elem = elem
+print(max_elem - min_elem)
