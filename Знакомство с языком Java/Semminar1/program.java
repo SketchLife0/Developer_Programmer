c8ba253e2cc8011e4ba3457389b14304423ea1e9
Artem Eremenko <qgren@ukr.net>
2023-01-25 16:49:47 +0200

    Separation into functions

--- Знакомство с языком Java/Semminar1/program.java
+++ Знакомство с языком Java/Semminar1/program.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;;
 public class program {
     public static void main(String[] args) {
+        int num = inputNum();
+        int result  = formula(num);
+        System.out.printf("Ответ %d", result);
+        
+    }
+
+    public static int inputNum(){
         Scanner scan = new Scanner(System.in);
         System.out.printf("Введите треугольное число: ");
-        int num = scan.nextInt();
-        num  = (num * ++num)/2;
-        System.out.printf("Ответ %d", num);
-        scan.close();
+        return scan.nextInt();
+    }
+
+    public static int formula(int x){
+        return (x * ++x)/2;
     }
 }
